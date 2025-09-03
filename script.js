cad9f94e298c06afb90875c1ba21f7b392201e41
Luis11prog <m_luisskt8@outlook.com>
2025-09-03 20:31:27 +0000

    Lineamientos de codigo. ·Consistencia en la identacion ·Se borro una etiqueta div de mas al final del html ·En el archivo JS unifique todas las funciones en una sola ·Correccion de un bug en el .fa-regular.fa-peper-plane a causa del CSS

--- script.js
+++ script.js
@@ -1,47 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
-   
-    const heartIcons = document.querySelectorAll('.fa-regular.fa-heart');
-    if (heartIcons.length > 0) {
-        heartIcons.forEach(heartIcon => {
-            heartIcon.addEventListener('click', () => {
-                heartIcon.classList.toggle('fa-solid');
-                
+    const toggleIcons = (selector) => {
+        document.querySelectorAll(selector).forEach(icon => {
+            icon.addEventListener('click', () =>{
+                icon.classList.toggle('fa-solid')
             });
         });
-    }
+    };
+toggleIcons('.fa-regular.fa-heart');
+toggleIcons('.fa-regular.fa-bookmark');
+toggleIcons('.fa-regular.fa-comment');
+toggleIcons('.fa-regular.fa-paper-plane');
 
-    
-    const bookmarkIcons = document.querySelectorAll('.fa-regular.fa-bookmark');
-    if (bookmarkIcons.length > 0) {
-       
-        bookmarkIcons.forEach(bookmarkIcon => {
-          
-            bookmarkIcon.addEventListener('click', () => {
-                
-                bookmarkIcon.classList.toggle('fa-solid');
-            });
-        });
-    }
-    const commentIcons = document.querySelectorAll('.fa-regular.fa-comment');
-    if (commentIcons.length > 0) {
-       
-        commentIcons.forEach(commentIcon => {
-          
-            commentIcon.addEventListener('click', () => {
-                
-                commentIcon.classList.toggle('fa-solid');
-            });
-        });
-    }
-    const planeIcons = document.querySelectorAll('.fa-regular.fa-paper-plane');
-    if (planeIcons.length > 0) {
-       
-        planeIcons.forEach(planeIcon => {
-          
-           planeIcon.addEventListener('click', () => {
-                
-                planeIcon.classList.toggle('fa-solid');
-            });
-        });
-    }
 });
